d2cc8fc4916554bd907b1be2d4eae8f3de7b9e08
terencematsune <matsuneterence@gmail.com>
2023-05-16 22:24:39 -0700

    margin

--- src/pages/homepage/homePage.tsx
+++ src/pages/homepage/homePage.tsx
@@ -3,27 +3,39 @@ import STUDY from "../../images/study.svg"
 import HOME1 from "../../images/home2.webp"
 import HOME2 from "../../images/home3.jpeg"
 import CAL from "../../images/cal.jpeg"
-import COMP from "../../images/shop.svg"
+import COMP from "../../images3/group-of-happy-business-people.svg"
 import { profileImgs } from "../../utils/profileImages"
 import { useNavigate } from "react-router"
+import { useEffect, useState } from "react"
 
 function HomePage(){//<img alt="" src={BLUE} id="blue"></img>
     const navigate = useNavigate();
+    var [dist, setDist] = useState(300);
+    const [imgType, setImgType] = useState(3)
 
     const goToLog = () => {
         navigate('/login')
     }
+    const test = () => {
+        for(let i = 0; i < 1304; i++){
+            setTimeout(() => {
+                setDist(dist + i)
+            }, 10 * i )
+        };
+    }
+    useEffect(() => {
+        test()
+    }, [])
+
     return (
         <div id="homeMain">
             <div id="homeContainer">
-                <div id="homeWrapper">
-                    <h1>STOIGOAL</h1>
-                    <p id="titleP">The best web app for tracking your progress <br></br>and keeping you on track</p>
-                    <span id="getStarted" onClick={goToLog}>Get Started</span>
-                </div>            
-                <div>
-                    <img alt="" src={STUDY} id="study"></img>
-                </div>
+                <h1>STOIGOAL</h1>
+        
+                <p id="titleP">The best web app for tracking your progress <br></br>and keeping you on track</p>
+                <span id="getStarted" onClick={goToLog}>Get Started</span>
+
+                <div style={{backgroundImage:`url(${COMP})`}} id="bgimage"> </div>
             </div>
             <div id="homeContainer2">
                 <div id="homeCard">
@@ -76,3 +88,29 @@ function HomePage(){//<img alt="" src={BLUE} id="blue"></img>
 }
 
 export default HomePage
+/*
+    #homeContainer{
+        display: flex;
+        margin-top: 10%;
+    }
+
+            <div id="homeContainer">
+                <div id="homeWrapper">
+                    <h1>STOIGOAL</h1>
+                    <p id="titleP">The best web app for tracking your progress <br></br>and keeping you on track</p>
+                    <span id="getStarted" onClick={goToLog}>Get Started</span>
+                </div>            
+                <div id="imgWrapper">
+                    <img alt="" src={STUDY} id="study"></img>
+                </div>
+            </div>
+
+        content: '';
+        position: absolute;
+        height: 560px;
+        width: 580px;
+        background-color: dodgerblue;
+        transform: translate(74.8%, -56%);
+        z-index: 2;
+        border-radius: 90% 74% 98% 60%;
+*/
